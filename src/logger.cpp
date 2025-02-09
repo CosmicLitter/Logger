024671eb43894d6f4751b8629a218c1f4d90056b
Brandon Holt <holtbd91@gmail.com>
2025-02-09 14:37:17 -0500

    cleanup

--- src/logger.cpp
+++ src/logger.cpp
@@ -13,29 +13,29 @@ LogEntry::LogEntry(LogLevel l, const std::string &m)
 
 void Logger::info(const std::string &message) {
   std::cout << getCurrentTimestamp() << " - " << fg::green << "[INFO]" 
-    << fg::reset << ": " << message
-    << std::endl;
+            << fg::reset << ": " << message << std::endl;
+
   messages.emplace_back(LogLevel::INFO, message);
 }
 
 void Logger::warn(const std::string &message) {
   std::cout << getCurrentTimestamp() << " - " << fg::yellow << "[WARN]" 
-    << fg::reset << ": " << message
-    << std::endl;
+            << fg::reset << ": " << message << std::endl;
+
   messages.emplace_back(LogLevel::WARN, message);
 }
 
 void Logger::error(const std::string &message) {
   std::cout << getCurrentTimestamp() << " - " << fg::red << "[ ERR]" 
-    << fg::reset << ": " << message
-    << std::endl;
+            << fg::reset << ": " << message << std::endl;
+
   messages.emplace_back(LogLevel::ERR, message);
 }
 
 void Logger::critical(const std::string &message) {
   std::cout << getCurrentTimestamp() << " - " << bg::red <<style::bold
-    << "[CRIT]" << bg::reset << ": " << message
-    << style::reset << std::endl;
+            << "[CRIT]" << bg::reset << ": " << message << style::reset << std::endl;
+
   messages.emplace_back(LogLevel::ERR, message);
 }
 
